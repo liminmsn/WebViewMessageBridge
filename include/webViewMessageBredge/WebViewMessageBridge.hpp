fc62e1f28abcfc0d265bffa88b3a122f020d794d
杨再涛 <1772081518@qq.com>
2026-09-25 09:50:24 +0800

    添加项目文件。

---
+++ include/webViewMessageBredge/WebViewMessageBridge.hpp
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <string>
+#include <nlohmann/json.hpp>
+#include <Windows.h>
+
+namespace WebViewMessageBridge {
+	std::string WideToUtf8(const wchar_t* value);
+
+	struct MessageData {
+		std::string value;
+	};
+
+	struct Message {
+		std::string id;
+		std::string type;
+		MessageData data;
+	};
+
+	enum class MessageEnum {
+		Unknown,
+		Info,
+		Http,
+		Client,
+		Warning,
+		Error
+	};
+
+	void from_json(const nlohmann::json& json, MessageData& data);
+	void from_json(const nlohmann::json& json, Message& message);
+
+	MessageEnum MessageEnumFromString(const std::string& type);
+	std::string MessageEnumToString(MessageEnum type);
+
+	using MessageHandler = std::function<void(Message&)>;
+	class WebViewMessageBridge
+	{
+	public:
+		WebViewMessageBridge() = default;
+		~WebViewMessageBridge() = default;
+		void Send(LPWSTR message);
+		void SetMessageHandler(MessageHandler handler);
+	private:
+		MessageHandler messageHandler;
+	};
+}
---
+++ src/WebViewMessageBridge.cpp
@@ -0,0 +1,81 @@
+﻿#include "../include/webViewMessageBredge/WebViewMessageBridge.hpp"
+
+namespace WebViewMessageBridge {
+	std::string WideToUtf8(const wchar_t* value)
+	{
+		if (!value)
+			return {};
+
+		int size = WideCharToMultiByte(CP_UTF8, 0, value, -1, nullptr, 0, nullptr, nullptr);
+
+		if (size <= 0)
+			return {};
+
+		std::string result(size - 1, '\0');
+
+		WideCharToMultiByte(CP_UTF8, 0, value, -1, result.data(), size, nullptr, nullptr);
+
+		return result;
+	}
+
+	void from_json(const nlohmann::json& json, MessageData& data)
+	{
+		data.value = json.value("message", "");
+	}
+
+	void from_json(const nlohmann::json& json, Message& message)
+	{
+		message.type = json.value("type", "");
+
+		if (json.contains("data") && json["data"].is_object()) {
+			message.data = json["data"].get<MessageData>();
+		}
+	}
+
+	MessageEnum MessageEnumFromString(const std::string& type)
+	{
+		if (type == "info")    return MessageEnum::Info;
+		if (type == "http")   return MessageEnum::Http;
+		if (type == "client") return MessageEnum::Client;
+		if (type == "warning") return MessageEnum::Warning;
+		if (type == "error")  return MessageEnum::Error;
+
+		return MessageEnum::Unknown;
+	}
+
+	std::string MessageEnumToString(MessageEnum type)
+	{
+		switch (type) {
+		case MessageEnum::Http:    return "http";
+		case MessageEnum::Client:  return "client";
+		case MessageEnum::Info:    return "info";
+		case MessageEnum::Warning: return "warning";
+		case MessageEnum::Error:   return "error";
+		default:                   return "unknown";
+		}
+	}
+
+	void WebViewMessageBridge::SetMessageHandler(MessageHandler handler)
+	{
+		messageHandler = std::move(handler);
+	}
+
+	void WebViewMessageBridge::Send(LPWSTR message)
+	{
+		try {
+			std::string jsonString = WideToUtf8(message);
+			nlohmann::json json = nlohmann::json::parse(jsonString);
+			Message message = json.get<Message>();
+			messageHandler(message);
+		}
+		catch (const nlohmann::json::exception&) {
+			// TODO: 处理非法 JSON
+			auto Unknown = Message{
+				"-1",
+				MessageEnumToString(MessageEnum::Unknown),
+				{"Json格式错误"}
+			};
+			messageHandler(Unknown);
+		}
+	}
+}
